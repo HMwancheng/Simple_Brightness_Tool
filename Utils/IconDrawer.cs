8910dbba4bb6a2a71a485e3736494cd83114eaf9
HMwancheng <91036325+HMwancheng@users.noreply.github.com>
2025-12-24 14:53:24 +0100

    Update IconDrawer.cs

--- Utils/IconDrawer.cs
+++ Utils/IconDrawer.cs
@@ -1,26 +1,20 @@
 using System.Drawing;
 using System.Drawing.Drawing2D;
+using System.Drawing.Text;
 using System.Windows.Forms;
 
 namespace SimpleBrightness.Utils
 {
     public static class IconDrawer 
     { 
-        public static Icon DrawSunIcon() 
+        public static Icon DrawNativeIcon() 
         { 
             using (Bitmap bmp = new Bitmap(32, 32)) using (Graphics g = Graphics.FromImage(bmp)) { 
                 g.SmoothingMode = SmoothingMode.AntiAlias; 
-                // 绘制实心圆
-                g.FillEllipse(Brushes.Gold, 10, 10, 12, 12); 
-                
-                // 绘制光芒
-                Pen rayPen = new Pen(Color.Gold, 2) { StartCap = LineCap.Round, EndCap = LineCap.Round }; 
-                for (int i = 0; i < 360; i += 45) { 
-                    double rad = i * Math.PI / 180; 
-                    float x1 = 16 + (float)(8 * Math.Cos(rad)); float y1 = 16 + (float)(8 * Math.Sin(rad)); 
-                    float x2 = 16 + (float)(14 * Math.Cos(rad)); float y2 = 16 + (float)(14 * Math.Sin(rad)); 
-                    g.DrawLine(rayPen, x1, y1, x2, y2); 
-                } 
+                g.TextRenderingHint = TextRenderingHint.ClearTypeGridFit;
+                Font iconFont = new Font("Segoe MDL2 Assets", 18, FontStyle.Regular);
+                // 确认偏移：-7, -1
+                TextRenderer.DrawText(g, "\uE706", iconFont, new Point(-7, -1), Color.White);
                 return Icon.FromHandle(bmp.GetHicon()); 
             } 
         } 
