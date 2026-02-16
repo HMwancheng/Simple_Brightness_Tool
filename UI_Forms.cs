67cb6e454c2a4b8864174d011e55cd051b4afff6
HMwancheng <HM_wancheng@outlook.com>
2026-02-17 00:38:35 +0100

    Use system TrackBar

--- UI_Forms.cs
+++ UI_Forms.cs
@@ -798,7 +798,7 @@ namespace SimpleBrightness
         private List<MonitorInfo> _monitors; 
         private AppConfig _config; 
         private MyCustomApplicationContext _context; 
-        private Dictionary<string, Win11TrackBar> _sliders = new Dictionary<string, Win11TrackBar>(); 
+        private Dictionary<string, TrackBar> _sliders = new Dictionary<string, TrackBar>(); 
         private Dictionary<string, Label> _valLabels = new Dictionary<string, Label>(); 
         private FlowLayoutPanel _mainPanel;
         
@@ -939,12 +939,15 @@ namespace SimpleBrightness
                 row1.Controls.Add(lblVal); 
                 card.Controls.Add(row1);
                 
-                Win11TrackBar slider = new Win11TrackBar { 
-                    Size = new Size(450, 28), 
+                // Use native Windows TrackBar for authentic system look
+                TrackBar slider = new TrackBar { 
+                    Size = new Size(450, 45), 
                     Maximum = 100, 
                     Minimum = 0, 
                     Value = m.LastBrightness, 
-                    Margin = new Padding(4, 0, 4, 8)
+                    Margin = new Padding(4, 0, 4, 8),
+                    TickStyle = TickStyle.None,
+                    Cursor = Cursors.Hand
                 }; 
                 _sliders[m.UniqueId] = slider; 
                 
